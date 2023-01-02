202561666402bc9c85d5f78fddf61a5096029984
Quan Cao <caoanhquan_qn@yahoo.com>
2023-01-02 20:27:19 +0700

    implement custom hook useDebounce

--- src/components/SearchBook.js
+++ src/components/SearchBook.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { search } from '../BooksAPI';
 import Book from './Book';
+import useDebounce from '../hooks/useDebounce';
 
 const SearchBook = ({ bookShelf }) => {
   const [strInput, setStrInput] = useState('');
@@ -9,35 +10,42 @@ const SearchBook = ({ bookShelf }) => {
   const handleChange = (e) => {
     setStrInput(e.target.value);
   };
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    search(strInput, 10).then((res) => {
-      const formattedData = res?.map((book) => {
-        const foundBook = bookShelf.find((b) => b.id === book.id);
-        if (foundBook) {
-          return {
-            ...foundBook,
-            style: {
-              width: 128,
-              height: 193,
-              backgroundImage: `url(${foundBook?.imageLinks?.smallThumbnail})`,
-            },
-          };
-        }
-        return {
-          ...book,
-          style: {
-            width: 128,
-            height: 193,
-            backgroundImage: `url(${book?.imageLinks?.smallThumbnail})`,
-          },
-        };
-      });
-      setList(formattedData);
-    });
-  };
+  const debouncedValue = useDebounce(strInput, 500);
+  useEffect(() => {
+    if (debouncedValue === '') {
+      setList([]);
+    } else {
+      search(debouncedValue, 10)
+        .then((res) => {
+          const formattedData = res?.map((book) => {
+            const foundBook = bookShelf.find((b) => b.id === book.id);
+            if (foundBook) {
+              return {
+                ...foundBook,
+                style: {
+                  width: 128,
+                  height: 193,
+                  backgroundImage: `url(${foundBook?.imageLinks?.smallThumbnail})`,
+                },
+              };
+            }
+            return {
+              ...book,
+              style: {
+                width: 128,
+                height: 193,
+                backgroundImage: `url(${book?.imageLinks?.smallThumbnail})`,
+              },
+            };
+          });
+          setList(formattedData);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [debouncedValue]);
+
   return (
-    <form className="search-books" onSubmit={handleSubmit}>
+    <form className="search-books">
       <div className="search-books-bar">
         <Link className="close-search" to="/">
           Close
